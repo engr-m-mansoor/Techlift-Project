c603a13806de33622a9a5902dc9b2d4381ac7047
engr-m-mansoor <muhammadmansoor417@gmail.com>
2023-03-04 18:17:16 +0500

    up

--- ATM-Management-System/src/main/java/Project/ATM/Management/System/Repositories/TransactionTypesRepository.java
+++ ATM-Management-System/src/main/java/Project/ATM/Management/System/Repositories/TransactionTypesRepository.java
@@ -1,9 +1,10 @@
 package Project.ATM.Management.System.Repositories;
 
+import Project.ATM.Management.System.Entities.TransactionType;
 import Project.ATM.Management.System.Entities.Withdraws;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionTypesRepository extends JpaRepository<TransactionTypesRepository, Integer> {
+public interface TransactionTypesRepository extends JpaRepository<TransactionType, Integer> {
 }
